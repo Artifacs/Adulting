5592100089d8c3ff51ebe730e2989c1648ee9077
Lesze <lesterandrew.arollado.cvt@eac.edu.ph>
2025-04-20 09:03:23 +0800

    Added Money Storing

--- First base code.py
+++ First base code.py
@@ -1,21 +1,25 @@
  #Hey! Welcome to my budgeting app, designed specifically for college students like me who are trying to manage their allowances and expenses.
  #Saving money doesn't mean we can't still have fun, right?
+
+MONEY = 0 
 print("Hello There Budgeting your finances as a college student for the future!")
-print("DO YOU WANT TO UPDATE YOUR BANK?:>  Y/N")
-Start = input()
+Start = input("DO YOU WANT TO UPDATE YOUR BANK?:>  Y/N: ")
 
+while Start != "Y".casefold():
+   Start = input("ERROR\nDO YOU WANT TO UPDATE YOUR BANK?:>  Y/N: ")
 if Start == 'Y'.casefold():
-
- allowance = input("ENTER AMOUNT: ")
-
+ allowance = int(input("ENTER AMOUNT: "))
+ MONEY += allowance
+ print(f"Your Money is: {MONEY}")
  try:
     allowance = float(allowance)  
     print(f"Your Allowance is: {allowance}")
     print("What is your daily expenses?")
-    Expenses = input("AMOUNT: ")
-    Expenses = float(Expenses)
+    Expenses = float(input("AMOUNT: "))
+    MONEY -= Expenses
+    print(f"Your Expenses daily is {Expenses} And your reamaining money is {MONEY}")
     
  except ValueError:  
-    print("PLEASE ENTER NUMBER ONLY")
+    print("PLEASE ENTER NUMBER ONLY")   
 else:
     print("HAVE A NICE DAY")
